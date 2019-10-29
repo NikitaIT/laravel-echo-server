5f220dc0a6fd7442bfe41c638b52d84746e88c65
NikitaIT <NIKITA2008-101@yandex.ru>
2019-10-29 20:11:36 +0300

    feat(RedisSubscriber): add dist

--- dist/subscribers/redis-subscriber.js
+++ dist/subscribers/redis-subscriber.js
@@ -2,7 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var Redis = require('ioredis');
 var log_1 = require("./../log");
-var zlib_1 = require("zlib");
 var RedisSubscriber = (function () {
     function RedisSubscriber(options) {
         this.options = options;
@@ -13,19 +12,13 @@ var RedisSubscriber = (function () {
         return new Promise(function (resolve, reject) {
             _this._redis.on('pmessage', function (subscribed, channel, message) {
                 try {
+                    if (_this.options.devMode) {
+                        log_1.Log.info(message);
+                    }
                     message = JSON.parse(message);
                     if (_this.options.devMode) {
                         log_1.Log.info("Channel: " + channel);
                         log_1.Log.info("Event: " + message.event);
-                    }
-                    if (_this.options.compressedPayload) {
-                        var event_1 = zlib_1.unzipSync(message.event, { level: 9 }).toString();
-                        var eventJSON = JSON.parse(event_1);
-                        if (_this.options.devMode) {
-                            log_1.Log.info("Event unziped string: " + event_1);
-                            log_1.Log.info("Event JSON: " + eventJSON);
-                        }
-                        message.event = eventJSON;
                     }
                     callback(channel, message);
                 }
